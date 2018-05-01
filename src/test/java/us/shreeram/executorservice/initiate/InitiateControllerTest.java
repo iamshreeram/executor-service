90673494e817ccede7ce49d57f832bea09939af9
iamshreeram <shreeram.v@live.com>
2018-05-01 11:12:02 -0600

    Adding Junit Test case

---
+++ src/test/java/us/shreeram/executorservice/initiate/InitiateControllerTest.java
@@ -0,0 +1,25 @@
+package us.shreeram.executorservice.initiate;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import us.shreeram.executorservice.Executor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Executor.class)
+public class InitiateControllerTest {
+    @Autowired
+    private InitiateController controller;
+
+    @Test
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
+
+}
