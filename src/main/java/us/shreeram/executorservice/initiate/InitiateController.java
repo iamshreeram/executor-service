b540f6e5b374b177e13ad5db234749bf18370c2e
iamshreeram <shreeram.v@live.com>
2018-05-02 10:57:26 -0600

    Adding Comments to service

--- src/main/java/us/shreeram/executorservice/initiate/InitiateController.java
+++ src/main/java/us/shreeram/executorservice/initiate/InitiateController.java
@@ -11,30 +11,44 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class InitiateController {
+
+    // Creating a /start request mapping
     @RequestMapping("/start")
     public String initiateScript(
+
+            //Creating query param with script name and path as header
             @RequestParam("scriptname") String scriptName,
             @RequestHeader("path") String path
     ) {
         final List<String> cmdList = new ArrayList<String>();
         final String script;
+
+        //Creating Script path
         script = path+scriptName;
+
+        // Adding commands to list
         cmdList.add(script);
 
         final ProcessBuilder pb = new ProcessBuilder(cmdList);
         try {
                 pb.redirectErrorStream(true);
+
+                //  Starting a process in server
                 Process p = pb.start();
+
+                //  Initiate the buffer reader to get the console output and print it in spring Logs
                 BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream()));
                 String line;
                 while((line = reader.readLine()) != null) {
                     SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
                     Date date = new Date();
                     System.out.println(formatter.format(date)+"   "+line);
+
                 }
+            return "Started the backend" +  script +" process.";
             } catch (IOException e) {
                 e.printStackTrace();
+                return script +" process failed to start.";
             }
-        return "Started the backend" +  script +" process.";
     }
 }
