11e3ce3533f23517ed5836df0b30b2fbde8eea1f
iamshreeram <shreeram.v@live.com>
2018-10-19 18:20:51 -0600

    Fixing Issue #1 - Taking the complete path in header

--- src/main/java/us/shreeram/applications/executorservice/controller/InitiateController.java
+++ src/main/java/us/shreeram/applications/executorservice/controller/InitiateController.java
@@ -3,8 +3,8 @@ package us.shreeram.applications.executorservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,19 +18,17 @@ public class InitiateController {
     private ScriptExecutor se;
 
   /**
-   * This handler initiates the execution of a script given the path and scriptname.
-   * 
-   * @param scriptName name of the script
+   * This handler initiates the execution of a script given the scriptname with full path.
    * @param path path of the script.
    * @return Status message.
    */
-    @RequestMapping("/start")
-  public String initiateScript(@RequestParam("scriptname") String scriptName, @RequestHeader("path") String path) {
-        final List<String> cmdList = new ArrayList<String>();
-        final String script;
-        script = path+scriptName;
-        cmdList.add(script);
-        String status = se.executeScript(cmdList);
-        return status;
-    }
+  //public String initiateScript(@RequestParam("scriptname") String scriptName, @RequestHeader("path") String path) {
+
+  @RequestMapping("/start")
+  public String initiateScript(@RequestHeader("path") String path) {
+      final List<String> cmdList = new ArrayList<String>();
+      cmdList.add(path);
+      String status = se.executeScript(cmdList);
+      return status;
+  }
 }
